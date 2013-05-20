94011def72a0632fc99cd7c0ebf31d900c0d556e
Cameron Dershem <cldershem@gmail.com>
2013-05-20 18:58:50 -0400

    some pages work

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 This is a browser based version of my battleship game.  It is awesome.
+
+Feature Yet to Come:
+    single player
+    multiplayer
+        place your own ships
+    login
+        follow users
+        find users to play
+            friends
+            randoms
+        track wins/losses
+        save game        
---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+CSRF_ENABLED = True
+SECRET_KEY = 'this-is-a-secret'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+# email server
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'cldershem@gmail.com'
+MAIL_PASSWORD = 'lnhzluiapptodxwt'
+
+# administrator list
+ADMINS = ['cldershem+battleshipweb@gmail.com']
+
